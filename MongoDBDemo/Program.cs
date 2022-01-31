a4fe77f6f46fa495cbc262d16496681717d0c79a
Marlyson Mendes <marlysonmendes@gmail.com>
2022-01-31 15:29:20 -0300

    conectando e criando o db MongoDB

--- MongoDBDemo/Program.cs
+++ MongoDBDemo/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MongoDB.Driver;
+using MongoDBDemo;
+
+string connectionString = "mongodb://127.0.0.1:27017";
+string databaseName = "simple_db";
+string collectionName = "people";
+
+var client = new MongoClient(connectionString);
+var db = client.GetDatabase(databaseName);
+var collection = db.GetCollection<PersonModel>(collectionName);
+
+var person = new PersonModel { FirstName = "Matheus", LastName = "Mendes" };
+
+await collection.InsertOneAsync(person);
+
+var results = await collection.FindAsync(_=>true);
+
+foreach (var result in results.ToList())
+{
+    Console.WriteLine($"{result.Id}: {result.FirstName} {result.LastName}");
+}
