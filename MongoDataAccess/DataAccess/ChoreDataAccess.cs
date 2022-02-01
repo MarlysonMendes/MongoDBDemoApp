e8153b9a7b9dc7c5b4ccf6fe36a716199d89d2d4
Marlyson Mendes <marlysonmendes@gmail.com>
2022-02-01 01:58:33 -0300

    Criação do projeto DataAccess
    Refatorando o projeto

---
+++ MongoDataAccess/DataAccess/ChoreDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using MongoDataAccess.Models;
+using MongoDB.Driver;
+namespace MongoDataAccess.DataAccess
+{
+    public class ChoreDataAccess
+    {
+        private const string ConnectionString = "mongodb://127.0.0.1:27017";
+        private const string databaseName = "choredb";
+        private const string ChoreCollection = "chore_chart";
+        private const string UserCollecion = "users";
+        private const string ChoreHistoryCollection = "chore_history";
+
+        private IMongoCollection<T> ConnectionToMongo<T>(string collection)
+        {
+            var client = new MongoClient ();
+            var db = client.GetDatabase (databaseName);
+            return db.GetCollection<T>(collection);
+        }
+    
+    }
+}
